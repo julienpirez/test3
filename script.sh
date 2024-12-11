105d6c77d17f962553578abb5110391879a43bbf
julienpirez <julien.pirez@hotmail.com>
2024-12-11 16:21:14 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# affichage d'un message dans la console
+echo "ou stocker le menu"
+read MENU
+cd 'C:/users/Julien P/Desktop/Git/test3'
+mkdir collection
+touch collection.txt
+echo "quel est le nom du film a ajouter"
+read FILM
+echo '$FILM'>collection.txt
+
+
+
+
+
